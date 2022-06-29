d71642c601f1e21a443051d3eadaae99f378b30c
IchorAmoc <vildebertelsen@gmail.com>
2022-06-29 20:19:16 +0200

    Fixed media q's 'n smaller things
    - Will rerender content when viewport changes (no more need to resfresh)
    - Can't figure out the font size thing news pages have, so it's looking meh now
    
    Ca worktime 1h

--- bodyv1/src/components/AllArticles.jsx
+++ bodyv1/src/components/AllArticles.jsx
@@ -6,16 +6,37 @@ import { Loading } from './Loading';
 
 export const AllArticles = () => {
     let [data, setData] = useState(null)
+    const [dimensions, setDimensions] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    })
 
-    // Fetch all data
-    // For each column, column component -> Full width 12 grid width
-    // For each article, article component
+    // Rerender content when viewport changes. Taken from Stackoverflow -- https://stackoverflow.com/questions/62846043/react-js-useeffect-with-window-resize-event-listener-not-working
+    // console.log(dimensions)
+    const handleResize = () => {
+        setDimensions({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize, false);
+      }, [])
+
+    // Fetch data (all articles)
     useEffect(() => {
         fetch("https://storage.googleapis.com/aller-structure-task/test_data.json")
             .then(res => res.json())
             .then(data => {
-                // console.log(data[0]) // Columns
-                // console.log(data[0].map(item => console.log('---', item))) // Columns
+                // console.log(data[0])
+                // console.log(data[0].map(row => {
+                //     console.log('Row |', row)
+                //     console.log('With columns/articles: ')
+                //     row.columns.map(column => console.log(column))
+                //     console.log('---------')
+                // }
+                // ))
 
                 setData(data[0])
             })
--- bodyv1/src/components/Article.jsx
+++ bodyv1/src/components/Article.jsx
@@ -7,20 +7,23 @@ import { IoIosSettings } from "react-icons/io";
 import { TitleChange } from "./TitleChange";
 
 export const Article = ({ data }) => {
+    
+
     const [edit, toggleEdit] = useState(false)
     const orgTitle = data.title
     const [title, setTitle] = useState(data.title)
     let width;
-    
+
 
     // There is 100% a better way to do this, but yeah.
-    if(window.innerWidth >= 1250) width = (data.width * 100)
-    if(window.innerWidth <= 1220) width = (data.width * 70) 
-    if(window.innerWidth <= 900) width = (data.width * 60) 
-    if(window.innerWidth <= 750) width = (data.width * 50) 
-    if(window.innerWidth <= 630) width = (data.width * 40) 
-    if(window.innerWidth <= 510) width = (data.width * 30)  // Uuuuh
-    if(window.innerWidth <= 400) width = (data.width * 20)  // Good god no
+    if (window.innerWidth >= 1250) width = (data.width * 100)
+    if (window.innerWidth <= 1220) width = (data.width * 70)
+    if (window.innerWidth <= 900) width = (data.width * 60)
+    if (window.innerWidth <= 750) width = (data.width * 50)
+    if (window.innerWidth <= 630) width = (data.width * 40)
+    if (window.innerWidth <= 510) width = (data.width * 30)
+    if (window.innerWidth <= 400) width = (data.width * 25)
+    if (window.innerWidth <= 330) width = (data.width * 20)
 
     // Changes only visible for the user,
     // API call to change for all users.
